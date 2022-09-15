9f20a908abca67fe3094f3f52782336f10749d51
Thedoc <thedocscripts@gmail.com>
2022-09-15 11:58:14 +0200

    tt

--- EnriqueJavier/src/main/java/ifp/enriquejavier/main.java
+++ EnriqueJavier/src/main/java/ifp/enriquejavier/main.java
@@ -1,20 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ifp.enriquejavier;
+
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author thedoc
  */
-public class main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class main extends JavaPlugin {
     
 }
